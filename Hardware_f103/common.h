75d0f2d5502975310d4316a8aaccb93d866a5049
Alexey <nine-4lex@mail.ru>
2019-08-20 17:24:00 +0300

    [hard] Removed firmware for F103

--- Hardware_f103/common.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMMON_H_
-#define COMMON_H_
-
-#define CLIP_VALUE(x, min, max) ( (x) < (min) ? (min) :     \
-                                  (x) > (max) ? (max) : (x) )
-#define SERVO_COUNT 2
-
-#endif // COMMON_H_
